2adf689744898d59a7541e23282239939d3b4d48
Pawel <pawel.urbanik.dev@gmail.com>
2026-04-25 19:34:05 +0200

    Initialized device registry service

---
+++ services/device-registry-service/src/test/java/pl/pawel/services/deviceregistryservice/DeviceRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pawel.services.deviceregistryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceRegistryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
